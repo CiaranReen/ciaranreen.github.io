0f6beddf39eb2bc4c4a3a5e5fd598f53ae05d4a0
Alainia, Ciaran <ciaran.reen@sky.uk>
2016-07-16 01:17:58 +0100

    add styles

--- Gemfile
+++ Gemfile
@@ -1,3 +1,2 @@
 source 'https://rubygems.org'
 gem 'jekyll-assets'
-gem 'react-jsx-sprockets'
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,6 +1,1 @@
 require "jekyll-assets"
-require "react-jsx-sprockets"
-
-ReactJSXSprockets.configure do |config|
-  config.extensions = %w( jsx )
-end
